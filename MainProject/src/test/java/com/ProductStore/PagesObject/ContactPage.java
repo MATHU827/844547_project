3503c89e5df6d7bbfdbecb18dfead215c2401b3f
MATHU827 <47879653+MATHU827@users.noreply.github.com>
2020-03-26 17:41:26 +0530

    commit 2

--- MainProject/src/test/java/com/ProductStore/PagesObject/ContactPage.java
+++ MainProject/src/test/java/com/ProductStore/PagesObject/ContactPage.java
@@ -1,9 +1,14 @@
 package com.ProductStore.PagesObject;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -64,14 +69,28 @@ public class ContactPage {
 		driver.findElement(contact_message).sendKeys(message);
 	}
 	//To click the send button to contact
-	public void sendMessage() throws InterruptedException
+	public void sendMessage() throws InterruptedException, Throwable
 	{
 		driver.findElement(send).click();
-		Thread.sleep(3000);
+		Thread.sleep(2000);
 		Alert a = driver.switchTo().alert();
-		  a.accept();
+		a.accept();
+		Thread.sleep(3000);
+		TakesScreenshot ts=(TakesScreenshot)driver;
+	    File ff=ts.getScreenshotAs(OutputType.FILE);
+	    FileUtils.copyFile(ff,new File("src\\test\\resources\\Screenshot\\contact.png"));
+			 
+			
 	}
-	//To exit from browser
+	//To take screenshot 
+	public void screenShot(String path) throws IOException, Throwable
+	{
+        Thread.sleep(4000);
+		TakesScreenshot ts=(TakesScreenshot)driver;
+		File ff=ts.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(ff,new File(path));
+	}
+   //To exit from browser
 	public void quit()
 	{
 		driver.close();
