192a4ec9dae1b9ac1b0ffd59a819908838cd9d80
MATHU827 <47879653+MATHU827@users.noreply.github.com>
2020-03-26 17:38:46 +0530

    commit 2

--- MainProject/src/test/java/com/ProductStore/PagesObject/AddToCartPage.java
+++ MainProject/src/test/java/com/ProductStore/PagesObject/AddToCartPage.java
@@ -1,9 +1,14 @@
 package com.ProductStore.PagesObject;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -61,6 +66,14 @@ public void cart() throws InterruptedException
 	Alert a = driver.switchTo().alert();
 	  a.accept();
 }
+//To take screenshot 
+public void screenShot(String path) throws IOException, Throwable
+{
+    Thread.sleep(4000);
+	TakesScreenshot ts=(TakesScreenshot)driver;
+	File ff=ts.getScreenshotAs(OutputType.FILE);
+	FileUtils.copyFile(ff,new File(path));
+	}
 
 //To close the browser 
 public void quit()
