af368fbaa05244997ca9972f7df136ce20b5b90a
MATHU827 <47879653+MATHU827@users.noreply.github.com>
2020-03-26 17:41:44 +0530

    Update LoginPage.java

--- MainProject/src/test/java/com/ProductStore/PagesObject/LoginPage.java
+++ MainProject/src/test/java/com/ProductStore/PagesObject/LoginPage.java
@@ -1,8 +1,13 @@
 package com.ProductStore.PagesObject;
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -68,6 +73,15 @@ public class LoginPage {
 	{
 	 driver.findElement(By.id("logout2")).click();
 	}
+	//To take screenshot 
+	public void screenShot(String path) throws IOException, Throwable
+	{
+        Thread.sleep(4000);
+		TakesScreenshot ts=(TakesScreenshot)driver;
+		File ff=ts.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(ff,new File(path));
+	}
+
 //To close driver
 	public void quit()
 	{
